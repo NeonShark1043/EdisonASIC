968587ae3b90d5df70c7ce946e14164f93c431a7
Ninh Tan Nguyen <nguy1380@eceprog1.ecn.purdue.edu>
2026-03-21 11:18:37 -0400

    refined button controls

--- verilog/rtl/edison/light_button.sv
+++ verilog/rtl/edison/light_button.sv
@@ -1,28 +1,4 @@
 `timescale 1ns / 1ps
-module edge_detector(
-    input logic clk,
-    input logic reset,
-    input logic signal_in,
-    output logic rising_edge,
-    output logic falling_edge
-);
-    logic signal_d, signal_e;
-
-    always_ff @(posedge clk or posedge reset) begin
-        if(reset) begin
-            signal_d <= 0;
-            signal_e <= 0;
-        end else begin
-            signal_d <= signal_in;
-            signal_e <= signal_d;
-        end
-    end
-
-    assign rising_edge = signal_d & ~signal_e;
-    assign falling_edge = ~signal_d & signal_e;
-
-endmodule
-
 /* SoC Button Controls
 1) Kickstart SoC: Hold Push Button
 2) Zero Processing Thresholds: Press Blank Button
